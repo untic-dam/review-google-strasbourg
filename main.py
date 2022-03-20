2e6de01e6e82a639105433c61bec9320a82590b3
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-20 12:57:04 +0100

    lecture data_csv et mise en place du sidebar

--- main.py
+++ main.py
@@ -13,6 +13,35 @@ st.write("""
     L'objectif est de visualiser l'ensemble des notes récoltées sur Google Maps
 """)
 
+#---------------------------------------------------------------------#
+#----------------------------[ Load DF ]------------------------------#
+#---------------------------------------------------------------------#
+
+df = pd.read_csv(v.data_csv)
+st.dataframe(df)
+
+#---------------------------------------------------------------------#
+#---------------------------[ Side Bar ]------------------------------#
+#---------------------------------------------------------------------#
+
+st.sidebar.header('Recherche')
+
+#---- Recherche par notes
+st.sidebar.write("Recherche Par Note :")
+note_min = st.sidebar.slider(label="note minimale :", 
+                            min_value=v.note_min, 
+                            max_value=v.note_max, 
+                            step=v.note_step)
+
+note_max = st.sidebar.slider(label="note maximale :", 
+                            min_value=v.note_min, 
+                            max_value=v.note_max, 
+                            step=v.note_step)
+
+#---- Recherche par nombre de review
+
+#---- Recherche par rue
+
 
 #---------------------------------------------------------------------#
 #------------------------------[ M A P ]------------------------------#
@@ -23,3 +52,5 @@ carte = folium.Map(location=v.strasbourg_loc, zoom_start=v.zoom_start)
 
 #affichage statique
 folium_static(carte)
+
+
--- variables.py
+++ variables.py
@@ -1,3 +1,13 @@
 #Carte
 strasbourg_loc = [48.58172649898996, 7.750611183023074]
 zoom_start = 14
+
+#DataFrame
+data_csv = 'restaurants_df_clean.csv'
+
+
+#slider 
+#recherche par notes
+note_min = 1.0
+note_max = 5.0
+note_step = 0.1

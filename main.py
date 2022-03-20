24d738889eb7ff29b74e38c449401f8b478fa394
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-20 16:44:36 +0100

    on peut afficher les resataurants en fonction de la rue

--- main.py
+++ main.py
@@ -26,7 +26,35 @@ st.write("""
 
 df = pd.read_csv(v.data_csv)
 
+def creation_df_rue(df):
 
+    #groupby
+    s_rue_rating_count = df.groupby(['rue'])['rating'].count()
+    s_rue_rating_mean = df.groupby(['rue'])['rating'].mean()
+    s_rue_rating_nreview = df.groupby(['rue'])['user_ratings_total'].sum()
+    s_rue_rating_median = df.groupby(['rue'])['rating'].median()
+    s_rue_rating_max = df.groupby(['rue'])['rating'].max()
+    s_rue_rating_min = df.groupby(['rue'])['rating'].min()
+
+    #Series to dataframe
+    cols = [s_rue_rating_count, s_rue_rating_mean, s_rue_rating_nreview,
+            s_rue_rating_median, s_rue_rating_max, s_rue_rating_min]
+    df_rue = pd.concat(cols, axis=1)
+
+    #rename cols
+    df_rue.columns.values[0] = 'restaurants_par_rue'
+    df_rue.columns.values[1] = 'moyenne_par_rue'
+    df_rue.columns.values[2] = 'nreview_par_rue'
+    df_rue.columns.values[3] = 'mediane_par_rue'
+    df_rue.columns.values[4] = 'max_par_rue'
+    df_rue.columns.values[5] = 'min_par_rue'
+
+
+    df_rue.sort_values(by=['restaurants_par_rue'], ascending=False, inplace=True)
+    
+    return df_rue
+
+df_resto_par_rue = creation_df_rue(df)
 
 #---------------------------------------------------------------------#
 #---------------------------[ Side Bar ]------------------------------#
@@ -53,13 +81,33 @@ nombre_min = int(st.sidebar.text_input('nombre minimum', 1))
 nombre_max = int(st.sidebar.text_input('nombre maximum', 1000))
 
 #---- Recherche par rue
+def creation_list_resto_sidebar(df_resto_par_rue):
 
+    list_resto = []
+    list_resto.append('aucune rue')
+
+    for index, row in df_resto_par_rue.iterrows():
+        name = index
+        n_resto = row['restaurants_par_rue']
+        moy = row['moyenne_par_rue']
+
+        txt = "{:.0f} resto(s) | {:.2f} * | {}".format(n_resto, moy, name)
+
+        list_resto.append(txt)
+
+    return list_resto
+
+list_resto = creation_list_resto_sidebar(df_resto_par_rue)
+
+rue_selected = st.sidebar.selectbox('rue :', list_resto)
+rue = rue_selected.split(' | ')[-1]
 
 
 #---------------------------------------------------------------------#
 #------------------------------[ M A P ]------------------------------#
 #---------------------------------------------------------------------#
 
+#fonction pour générer la carte
 def creation_df_carte(df, note_min, note_max, nombre_min, nombre_max, rue):
     df_min_max = df.copy()
 
@@ -72,8 +120,12 @@ def creation_df_carte(df, note_min, note_max, nombre_min, nombre_max, rue):
     df_min_max = df_min_max.loc[df_min_max['user_ratings_total']<=nombre_max]
 
     #par rue
-    if len(list(rue))>1:
-        df_min_max = df_min_max[df_min_max['rue']==rue]
+    try:
+        if len(list(rue))>1:
+            if rue != 'aucune rue':
+                df_min_max = df_min_max[df_min_max['rue']==rue]
+    except:
+        pass
     
     return df_min_max
 
@@ -95,10 +147,9 @@ def generate_carte(carte, df):
 #init
 carte = folium.Map(location=v.strasbourg_loc, zoom_start=v.zoom_start)
 
-df_carte = creation_df_carte(df, note_min, note_max, nombre_min, nombre_max, rue='')
+df_carte = creation_df_carte(df, note_min, note_max, nombre_min, nombre_max, rue)
 
 carte = generate_carte(carte, df_carte)
-
 
 #affichage statique
 folium_static(carte)

ea8c087be712220d02e669c3af6b0258e0c56af1
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-20 13:08:27 +0100

    affichage des restaurants en fonction de la note

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import folium
 
 import variables as v
 
+#---------------------------------------------------------------------#
+#------------------------------[ HEAD ]-------------------------------#
+#---------------------------------------------------------------------#
 
 st.title("Les Notes Google Maps des Restautants de Strasborg - Partie I")
 
@@ -13,12 +16,16 @@ st.write("""
     L'objectif est de visualiser l'ensemble des notes récoltées sur Google Maps
 """)
 
+
+
+
 #---------------------------------------------------------------------#
 #----------------------------[ Load DF ]------------------------------#
 #---------------------------------------------------------------------#
 
 df = pd.read_csv(v.data_csv)
-st.dataframe(df)
+
+
 
 #---------------------------------------------------------------------#
 #---------------------------[ Side Bar ]------------------------------#
@@ -39,8 +46,13 @@ note_max = st.sidebar.slider(label="note maximale :",
                             step=v.note_step)
 
 #---- Recherche par nombre de review
+st.sidebar.write("Recherche Par Nombre de Review :")
+nombre_min = st.sidebar.text_input('nombre minimum', 1)
+
+nombre_max = st.sidebar.text_input('nombre maximum', 1000)
 
 #---- Recherche par rue
+
 
 
 #---------------------------------------------------------------------#
@@ -49,6 +61,28 @@ note_max = st.sidebar.slider(label="note maximale :",
 
 #init
 carte = folium.Map(location=v.strasbourg_loc, zoom_start=v.zoom_start)
+
+def generate_carte_rating(carte, df, rating_min, rating_max):
+    
+    #range selection
+    df_rating_min = df.loc[df['rating']>=rating_min]
+    df_rating_min_max = df_rating_min.loc[df_rating_min['rating']<=rating_max]
+
+    for index, row in df_rating_min_max.iterrows():
+        lat = row['lat']
+        lon = row['lon']
+        name = row['name']
+        rating = row['rating']
+        n_rating = row['user_ratings_total']
+        txt_tooltip = f"{name} | {rating} * | {n_rating} évaluation(s)"
+        txt_popup = f"{name} {rating} * {n_rating} évaluation(s)"
+
+        folium.Marker([lat, lon], tooltip=txt_tooltip, popup=txt_popup).add_to(carte)
+
+
+    return carte
+
+carte = generate_carte_rating(carte, df, note_min, note_max)
 
 #affichage statique
 folium_static(carte)
